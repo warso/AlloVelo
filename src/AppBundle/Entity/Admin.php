45b99852c62331c55f3cbd38f90b301dd8927e8e
Formation <Formation@10.100.200.99>
2017-02-16 11:31:46 +0100

    modifs admin par Hélène

---
+++ src/AppBundle/Entity/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Admin
+ *
+ * @ORM\Table(name="admin")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AdminRepository")
+ */
+class Admin extends Utilisateur
+{
+
+}
