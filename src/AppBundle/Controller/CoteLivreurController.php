678cef0e78f733d535126795c5d3674a620beb8f
Formation <Formation@10.100.200.134>
2017-02-16 10:30:30 +0100

    MAJ coteLivreur + liste produit html par alex

--- src/AppBundle/Controller/CoteLivreurController.php
+++ src/AppBundle/Controller/CoteLivreurController.php
@@ -17,32 +17,12 @@ class CoteLivreurController extends Controller
     {
       $qb->select("c")
                 ->from("AppBundle:Commande", "c")
-                ->orderBy("r.id");
+                ->orderBy("c.id");
+      
         return $this->render('AppBundle:CoteLivreur:liste_produit.html.twig', array("mesCommandes"=> $commandes ));
             // ...
       
     }
 
 }
- public function listeDerniereRecetteAction($page = 0) {
-
-        $qb = $this->getDoctrine()->getManager()->createQueryBuilder();
-//        $qb = new \Doctrine\ORM\QueryBuilder;
-
-        $qb->select("r")
-                ->from("AppBundle:Recette", "r")
-                ->orderBy("r.id");
-//                ->setFirstResult(0)
-//                ->setMaxResults(2);
-
-
-        $query = $qb->getQuery();
-
-        $recettes = $query->getResult();
-
-        // Calcul nb de resultat
-        $nbPages = (int) (sizeof($recettes)); //ou count()
-
-
-    }
-
+ 
